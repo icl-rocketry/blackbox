9835c64bc17069798680bb2759d594046bdbd840
shreybohra <42218014+shreybohra@users.noreply.github.com>
2022-01-12 19:24:46 +0000

    increase batch size and parallelise

--- udp_plot.m
+++ udp_plot.m
@@ -2,15 +2,14 @@ clear
 [~, localIP] = system(['netsh interface ip show address | ' ...
 'findstr "IP Address" | findstr "192"']);
 
-localIP = strtrim(erase(localIP, "IP Address: "))
+localIP = strtrim(erase(localIP, "IP Address: "));
 
 u = udpport("byte", "LocalHost", localIP, "LocalPort", 20001);
-log = []
+log = [];
 datalen = 20;
 
 while true
-    if u.NumBytesAvailable > 0
-        u.NumBytesAvailable
+    if u.NumBytesAvailable >= 320
         rawdata = read(u, u.NumBytesAvailable, "single");
         fprintf('Data received\n');
         log = [log; separate(rawdata, datalen)];
@@ -19,14 +18,12 @@ end
 
 function new = separate(raw, cols)
     rows = length(raw)/cols;
-    fprintf('%i rows, %i cols', rows, cols)
+%     fprintf('%i rows, %i cols', rows, cols)
     new = zeros(rows, cols);
     
-    for i = 1:1:rows
+    parfor i = 1:1:rows
         for j = 1:1:cols
             new(i, j) = raw(((i-1)*cols)+j);
-        end
-        
-    end
-    
+        end        
+    end    
 end
