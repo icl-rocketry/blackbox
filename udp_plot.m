1ea85585fb6fe58734b899a47bbcd678720f816e
shreybohra <42218014+shreybohra@users.noreply.github.com>
2022-01-13 00:22:48 +0000

    decrease wait time to improve perf

--- udp_plot.m
+++ udp_plot.m
@@ -1,27 +1,41 @@
 clear
+g = 10;
+% profile clear
+% profile on -historysize 5000000000
+
 [~, localIP] = system(['netsh interface ip show address | ' ...
 'findstr "IP Address" | findstr "192"']);
 
 localIP = strtrim(erase(localIP, "IP Address: "));
 
 u = udpport("byte", "LocalHost", localIP, "LocalPort", 20001);
-log = [];
+log = zeros(1, 20);
 datalen = 20;
 
+
+% while true
+i = 0;
+u.NumBytesAvailable
+% tic
 while true
-    if u.NumBytesAvailable >= 320
-        rawdata = read(u, u.NumBytesAvailable, "single");
-        fprintf('Data received\n');
-        log = [log; separate(rawdata, datalen)];
+    if u.NumBytesAvailable >= 20
+%         u.NumBytesAvailable
+        rawdata = read(u, 20, "single");
+%         fprintf('Data received\n');
+        framed = separate(rawdata, datalen);
+        log = [log; framed];
+        i = i+1;
     end
 end
+% toc
+% profile viewer
 
 function new = separate(raw, cols)
     rows = length(raw)/cols;
 %     fprintf('%i rows, %i cols', rows, cols)
     new = zeros(rows, cols);
     
-    parfor i = 1:1:rows
+    for i = 1:1:rows
         for j = 1:1:cols
             new(i, j) = raw(((i-1)*cols)+j);
         end        
