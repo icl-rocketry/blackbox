f43261700daf36cc1d46236a69f524be146c96e7
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2022-01-15 11:20:06 +0000

    Moved data viewer into component

--- blackbox-frontend/src/App.svelte
+++ blackbox-frontend/src/App.svelte
@@ -1,20 +1,10 @@
 <script lang="ts">
+import Data from "./Data.svelte";
+
 </script>
 
 <main>
-  <div class="bg-slate-900 min-h-screen flex items-center justify-center">
-    <div class="grid grid-cols-4 gap-2 min-h-screen min-w-full">
-      <div class="p-1 bg-slate-800 text-white rounded-xl">1</div>
-      <div class="p-1 bg-slate-800 text-white rounded-xl">2</div>
-      <div class="p-1 bg-slate-800 text-white rounded-xl">3</div>
-      <div class="p-1 bg-slate-800 text-white rounded-xl">4</div>
-      <div class="p-1 bg-slate-800 text-white rounded-xl">5</div>
-      <div class="p-1 bg-slate-800 text-white rounded-xl row-span-2 col-span-2">6</div>
-      <div class="p-1 bg-slate-800 text-white rounded-xl">7</div>
-      <div class="p-1 bg-slate-800 text-white rounded-xl">8</div>
-      <div class="p-1 bg-slate-800 text-white rounded-xl">9</div>
-    </div>
-  </div>
+  <Data/>
 </main>
 
 <style global>
