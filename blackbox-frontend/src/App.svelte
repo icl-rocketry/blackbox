a1cf4261ca0779bb2afe4c32218e02fcbc866b54
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2022-01-15 10:24:35 +0000

    Initial svelte frontend

---
+++ blackbox-frontend/src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  $: counter = 0;
+</script>
+
+<main>
+  <div class="bg-red-500">
+    Count: {counter}
+  </div>
+  <button on:click={(_) => counter++}>Increment</button>
+</main>
+
+<style global>
+  @tailwind utilities;
+  @tailwind components;
+  @tailwind base;
+</style>
