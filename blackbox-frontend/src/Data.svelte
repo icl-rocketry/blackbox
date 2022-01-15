6b25e4b6b11ec3e455d569f60ec731ece95cd0c6
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2022-01-15 12:32:52 +0000

    Added chart component

--- blackbox-frontend/src/Data.svelte
+++ blackbox-frontend/src/Data.svelte
@@ -1,13 +1,22 @@
+<script>
+  import Chart from "./lib/Chart.svelte";
+</script>
+
 <div class="bg-slate-900 min-h-screen flex items-center justify-center">
-    <div class="grid grid-cols-4 gap-2 min-h-screen min-w-full">
-      <div class="p-1 bg-slate-800 text-white rounded-xl">1</div>
-      <div class="p-1 bg-slate-800 text-white rounded-xl">2</div>
-      <div class="p-1 bg-slate-800 text-white rounded-xl">3</div>
-      <div class="p-1 bg-slate-800 text-white rounded-xl">4</div>
-      <div class="p-1 bg-slate-800 text-white rounded-xl">5</div>
-      <div class="p-1 bg-slate-800 text-white rounded-xl row-span-2 col-span-2">6</div>
-      <div class="p-1 bg-slate-800 text-white rounded-xl">7</div>
-      <div class="p-1 bg-slate-800 text-white rounded-xl">8</div>
-      <div class="p-1 bg-slate-800 text-white rounded-xl">9</div>
+  <div class="grid grid-cols-4 gap-2 min-h-screen min-w-full">
+    <div class="bg-slate-800 text-white flex">
+      <Chart id="top_left"/>
     </div>
+
+    <div class="bg-slate-800 text-white flex justify-center items-center rounded-xl"><Chart id="1"/></div>
+    <div class="bg-slate-800 text-white flex justify-center items-center rounded-xl"><Chart id="2"/></div>
+    <div class="bg-slate-800 text-white flex justify-center items-center rounded-xl"><Chart id="3"/></div>
+    <div class="bg-slate-800 text-white flex justify-center items-center rounded-xl"><Chart id="4"/></div>
+    <div class="bg-slate-800 text-white rounded-xl row-span-2 col-span-2 min-h-full">
+      6
+    </div>
+    <div class="bg-slate-800 text-white flex justify-center items-center rounded-xl"><Chart id="5"/></div>
+    <div class="bg-slate-800 text-white flex justify-center items-center rounded-xl"><Chart id="6"/></div>
+    <div class="bg-slate-800 text-white flex justify-center items-center rounded-xl"><Chart id="7"/></div>
   </div>
+</div>
---
+++ blackbox-frontend/src/lib/Chart.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import Chart from "chart.js";
+
+  export let id: string;
+
+  onMount(renderChart);
+  function renderChart() {
+    var ctx = document.getElementById(id).getContext("2d");
+    var chart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July", "August"],
+        datasets: [
+          {
+            label: "My First dataset",
+            borderColor: "rgb(255, 99, 132)",
+            data: [0, 10, 5, 2, 20, 30, 45, 80]
+          }
+        ]
+      },
+      options: {}
+    });
+  }
+</script>
+<canvas id={id} class="object-cover max-h-full max-w-full"></canvas>
