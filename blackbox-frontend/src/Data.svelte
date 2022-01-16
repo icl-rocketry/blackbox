27ef5ef5b345ffcdfbc2f04b9a01b6d656a42a85
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2022-01-16 00:19:56 +0000

    Added chat updating and better rendering

--- blackbox-frontend/src/Data.svelte
+++ blackbox-frontend/src/Data.svelte
@@ -1,22 +1,100 @@
-<script>
+<script lang="ts">
   import Chart from "./lib/Chart.svelte";
+  import type { ChartData } from "./lib/Chart.svelte";
+
+  const labels = [];
+  const x = [];
+  const n = 100;
+  for (let i = 0; i < n; i++) {
+    labels.push(i);
+    x.push(Math.random() * 10);
+  }
+
+  let data: ChartData = {
+    datasets: [
+      {
+        label: "Dataset 1",
+        data: x,
+        borderColor: "#FF0000",
+      },
+    ],
+  };
+  const d2: ChartData = {
+    datasets: [
+      {
+        label: "Dataset 1",
+        data: [1, 2, 3, 4, 5, 6, 7],
+        borderColor: "#FF0000",
+      },
+      {
+        label: "Dataset 2",
+        data: [7, 6, 5, 4, 3, 2, 1],
+        borderColor: "#0000FF",
+      },
+    ],
+  };
+
+  setInterval(() => {
+    data = {
+      datasets: [
+        {
+          label: data.datasets[0].label,
+          data: [...data.datasets[0].data.slice(1), Math.floor(Math.random() * 10)],
+          borderColor: data.datasets[0].borderColor,
+        },
+      ],
+    };
+
+  }, 50);
 </script>
 
 <div class="bg-slate-900 min-h-screen flex items-center justify-center">
   <div class="grid grid-cols-4 gap-2 min-h-screen min-w-full">
-    <div class="bg-slate-800 text-white flex justify-center items-center rounded-xl">
-      <Chart id="top_left"/>
+    <div
+      class="bg-slate-800 text-white flex justify-center items-center rounded-xl"
+    >
+      <Chart id="top_left" {data} title="Acceleration"/>
     </div>
 
-    <div class="bg-slate-800 text-white flex justify-center items-center rounded-xl"><Chart id="1"/></div>
-    <div class="bg-slate-800 text-white flex justify-center items-center rounded-xl"><Chart id="2"/></div>
-    <div class="bg-slate-800 text-white flex justify-center items-center rounded-xl"><Chart id="3"/></div>
-    <div class="bg-slate-800 text-white flex justify-center items-center rounded-xl"><Chart id="4"/></div>
-    <div class="bg-slate-800 text-white rounded-xl row-span-2 col-span-2 min-h-full">
+    <div
+      class="bg-slate-800 text-white flex justify-center items-center rounded-xl"
+    >
+      <Chart id="1" data={d2} />
+    </div>
+    <div
+      class="bg-slate-800 text-white flex justify-center items-center rounded-xl"
+    >
+      <Chart id="2" data={d2} />
+    </div>
+    <div
+      class="bg-slate-800 text-white flex justify-center items-center rounded-xl"
+    >
+      <Chart id="3" data={d2} />
+    </div>
+    <div
+      class="bg-slate-800 text-white flex justify-center items-center rounded-xl"
+    >
+      <Chart id="4" data={d2} />
+    </div>
+    <div
+      class="bg-slate-800 text-white rounded-xl row-span-2 col-span-2 min-h-full"
+    >
       6
     </div>
-    <div class="bg-slate-800 text-white flex justify-center items-center rounded-xl"><Chart id="5"/></div>
-    <div class="bg-slate-800 text-white flex justify-center items-center rounded-xl"><Chart id="6"/></div>
-    <div class="bg-slate-800 text-white flex justify-center items-center rounded-xl"><Chart id="7"/></div>
+    <div
+      class="bg-slate-800 text-white flex justify-center items-center rounded-xl"
+    >
+      <Chart id="5" data={d2} />
+    </div>
+    <div
+      class="bg-slate-800 text-white flex justify-center items-center rounded-xl"
+    >
+      <Chart id="6" data={d2} />
+    </div>
+    <div
+      class="bg-slate-800 text-white flex justify-center items-center rounded-xl"
+    >
+      <Chart id="7" data={d2} />
+    </div>
   </div>
 </div>
--- blackbox-frontend/src/lib/Chart.svelte
+++ blackbox-frontend/src/lib/Chart.svelte
@@ -1,48 +1,99 @@
+<script context="module" lang="ts">
+  export type ChartData = {
+    datasets: {
+      label: string;
+      data: number[];
+      borderColor: string;
+    }[];
+  };
+</script>
+
+<!-- svelte-ignore non-top-level-reactive-declaration -->
 <script lang="ts">
-  import { onMount } from "svelte";
-  import { Chart } from "chart.js";
+  import { afterUpdate, onMount } from "svelte";
+  import { Chart, registerables } from 'chart.js';
+  Chart.register(...registerables);
+
 
   export let id: string;
+  export let data: ChartData;
+  export let title = "Title";
+  export const xMax: number = 100;
+  export const xMin: number = 0;
+  export const yMax: number = 10;
+  export const yMin: number = -10;
 
-  onMount(renderChart);
+  let chart: Chart;
 
   function renderChart() {
     let ctx = document.getElementById(id);
 
-    const labels = [1, 2, 3, 4, 5, 6, 7];
-    const data = {
-      labels: labels,
-      datasets: [
-        {
-          label: "Dataset 1",
-          data: [1, 2, 3, 4, 5, 6, 7],
-          borderColor: "#FF0000",
-          backgroundColor: "transparent",
-        },
-        {
-          label: "Dataset 2",
-          data: [7, 6, 5, 4, 3, 2, 1],
-          borderColor: "#0000FF",
-          backgroundColor: "transparent",
-        },
-      ],
+    const chart_data = {
+      labels: Array.from(Array(xMax-xMin)).map((_, i) => i + xMin),
+      datasets: data.datasets.map(({ label, data, borderColor }) => ({
+        label,
+        data,
+        borderColor,
+        backgroundColor: "transparent",
+        radius: 0,
+        tension: 0.1,
+        borderWidth: 1,
+      })),
     };
 
     const config = {
       type: "line",
-      data: data,
+      data: chart_data,
       options: {
-        responsive: true,
+        toolTip: false,
+        animation: false,
+        scales: {
+          xAxis: {
+            max: xMax,
+            min: xMin,
+            grid: {
+              drawBorder: false,
+              display: false,
+            },
+            type: "linear",
+          },
+          yAxis: {
+            max: yMax,
+            min: yMin,
+            grid: {
+              drawBorder: false,
+              display: false,
+            },
+          },
+        },
         plugins: {
           legend: {
             position: "bottom",
           },
+          title: {
+            display: true,
+            text: title,
+            color: "#FFFFFF"
+          }
         },
       },
     };
-
-    new Chart(ctx as any, config as any);
+    return new Chart(ctx as any, config as any);
   }
+
+  onMount(() => {
+    chart = renderChart();
+  });
+  afterUpdate(() => {
+    const chart_data = chart.data;
+    if (data.datasets.length > 0) {
+      chart_data.datasets.forEach((dataset: { data: any[] }, index: number) => {
+        dataset.data = data.datasets[index].data;
+      });
+
+      chart.update();
+    }
+  });
 </script>
 
 <canvas {id} class="object-cover max-h-full max-w-full" />
