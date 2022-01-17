d96226c2b12214883e27c188bbca8b1f8a6caac2
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2022-01-17 18:56:31 +0000

    Added text component

---
+++ blackbox-frontend/src/lib/Text.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import type { Readable } from "svelte/store";
+  export let generator: Readable<number>;
+  export let units: string;
+</script>
+
+<p class="text-white text-6xl font-mono">{$generator} {units}</p>
