d7d969f2a6074dfb67fe79edeba3e8b3cce7ca66
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2022-02-26 10:48:53 +0000

    Replaced slider

--- blackbox-frontend/src/Footer.svelte
+++ blackbox-frontend/src/Footer.svelte
@@ -1,18 +1,42 @@
 <script lang="ts">
   import Slider from "@bulatdashiev/svelte-slider";
-  let value = [50];
+  let value = 50;
 </script>
 
 <div class="bg-slate-900 h-20 flex flex-row justify-center">
-    <div class="flex flex-col justify-center w-full">
-        <Slider min="0" max="100" step="1" bind:value/>
-    </div>
+  <div class="slidecontainer w-full flex flex-col justify-center">
+    <input type="range" min="1" max="100" {value} class="slider" id="myRange" />
+  </div>
 </div>
 
 <style>
-  :root {
-    --track-bg: #ebebeb;
-    --progress-bg: red;
-    --thumb-bg: red;
+  .slider {
+    -webkit-appearance: none;
+    width: 100%;
+    height: 5px;
+    border-radius: 5px;
+    background: #d3d3d3;
+    outline: none;
+    opacity: 0.7;
+    -webkit-transition: 0.2s;
+    transition: opacity 0.2s;
+  }
+
+  .slider::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    appearance: none;
+    width: 15px;
+    height: 15px;
+    border-radius: 50%;
+    background: red;
+    cursor: pointer;
+  }
+
+  .slider::-moz-range-thumb {
+    width: 15px;
+    height: 15px;
+    border-radius: 50%;
+    background: red;
+    cursor: pointer;
   }
 </style>
